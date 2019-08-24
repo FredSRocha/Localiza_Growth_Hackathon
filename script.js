a7db626e9f6adf0bcf2aa6b583fdc23fe410b8a7
fredsrocha <fredsrocha1985@gmail.com>
2019-08-24 09:05:31 -0300

    initial code

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function calcular() {
+  var dailyMileage = parseFloat(document.querySelector("input[name='dailyMileage']").value);
+  var resultado = document.getElementById("resultado");
+  resultado.innerHTML = "Preço Diário (UBER): R$ " + dailyMileage * 1.89 + "<br> Preço Mensal (UBER): R$ " + (dailyMileage * 1.89) * 30 + "<br><small>*Simulação na categoria mais barata (UBERX) desconsiderando preço dinâmico e considerando trânsito ideal.</small>";
+}
+var form = document.getElementById("calculo");
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+  calcular();
+});
